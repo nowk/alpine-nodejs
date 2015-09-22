ec664c917264d785bf1900dcad12e6b9dda57ca2
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-09-22 04:29:05 +0000

    Change var name

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -e
 
-from=nowk/alpine-nodejs:4.1.0
+name=nowk/alpine-nodejs:4.1.0
 
 # remove .cid, incase there is a lingering one
 if [ -f .cid ] ; then
 	rm .cid
 fi
 
-docker build --rm -t $from .
-docker run --privileged=true --cidfile .cid $from /max_user_watches.sh
+docker build --rm -t $name .
+docker run --privileged=true --cidfile .cid $name /max_user_watches.sh
 
 cid=`cat .cid` # save cid to var
 rm .cid        # remove .cid
 
-docker commit -m "Update max_user_watches" -a "Yung Hwa Kwon" $cid $from
+docker commit -m "Update max_user_watches" -a "Yung Hwa Kwon" $cid $name
